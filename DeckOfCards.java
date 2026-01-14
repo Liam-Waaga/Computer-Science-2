a95961609d1b3af5a211803e2818ef914d9cb953
Liamwaaga <liamwaaga@proton.me>
2026-01-13 21:10:38 -0800

    done

---
+++ DeckOfCards.java
@@ -0,0 +1,40 @@
+import java.security.SecureRandom;
+
+public class DeckOfCards {
+
+    private static final SecureRandom randomNumbers = new SecureRandom();
+    private static final int NUMBER_OF_CARDS = 52;
+
+    private Card[] deck = new Card[NUMBER_OF_CARDS];
+    private int currentCard = 0;
+
+    public DeckOfCards() {
+        String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        for (int i = 0; i < deck.length; i++) {
+            deck[i] = new Card(faces[i % 13], suits[i / 13]);
+        }
+    }
+
+    public void shuffle() {
+        currentCard = 0;
+
+        for (int i = 0; i < deck.length; i++) {
+            int next = randomNumbers.nextInt(NUMBER_OF_CARDS);
+
+            Card tmp = deck[i];
+            deck[i] = deck[next];
+            deck[next] = tmp;
+        }
+    }
+
+    public Card dealCard() {
+        if (currentCard < deck.length) {
+            return deck[currentCard++];
+        } else {
+            return null;
+        }
+    }
+}
---
+++ Hand.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+
+public class Hand {
+    private ArrayList<Card> cards;
+    private boolean hasBust = false;
+    private boolean blackJack = false;
+
+    public Hand() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public Card getCard(int index) {
+        return cards.get(index);
+    }
+
+    public int calculateHand() {
+        int aces = 0;
+        int total = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            String val = cards.get(i).getFace();
+            if (val.equals("Ace"))
+                aces++;
+            else if (val.equals("Deuce"))
+                total += 2;
+            else if (val.equals("Three"))
+                total += 3;
+            else if (val.equals("Four"))
+                total += 4;
+            else if (val.equals("Five"))
+                total += 5;
+            else if (val.equals("Six"))
+                total += 6;
+            else if (val.equals("Seven"))
+                total += 7;
+            else if (val.equals("Eight"))
+                total += 8;
+            else if (val.equals("Nine"))
+                total += 9;
+            else if (val.equals("Ten"))
+                total += 10;
+            else if (val.equals("Jack"))
+                total += 10;
+            else if (val.equals("Queen"))
+                total += 10;
+            else if (val.equals("King"))
+                total += 10;
+        }
+        total += aces;
+        while (aces > 0) {
+            if (total + 10 <= 21) {
+                aces--;
+                total += 10;
+            } else {
+                break;
+            }
+        }
+        return total;
+    }
+
+    public String toString() {
+        if (cards.size() == 0)
+            return "nothing";
+        String out = "a " + cards.get(0).toString();
+        for (int i = 1; i < cards.size(); i++) {
+            out += " and a " + cards.get(i).toString();
+        }
+
+        return out;
+    }
+
+    public void bust() {
+        this.hasBust = true;
+    }
+
+    public void blackJack() {
+        this.blackJack = true;
+    }
+
+    public boolean hasBust() {
+        return this.hasBust;
+    }
+
+    public boolean hasBlackjack() {
+        return this.blackJack;
+    }
+}
