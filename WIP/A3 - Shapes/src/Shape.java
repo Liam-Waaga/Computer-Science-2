66af6cbf097e889a330c5cd209231374cfd13aba
Liamwaaga <liamwaaga@proton.me>
2026-01-23 22:05:21 -0800

    start Animals and Shapes

---
+++ WIP/A3 - Shapes/src/Shape.java
@@ -0,0 +1,9 @@
+
+public interface Shape {
+
+    /*
+     * Must return the number of "units" contained
+     * if it is a 2d shape, then it is area, if it is 3d then it is volume
+     */
+    public double unitsContained(Point point);
+}
---
+++ WIP/A3 - Shapes/src/Tetrahedron.java
@@ -0,0 +1,3 @@
+public class Tetrahedron extends Polyhedron {
+    
+}
